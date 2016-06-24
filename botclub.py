164e33ff55d5503358c8c882061421763aeb6411
Sergi Gilabert Sempere <05104.casp@fje.edu>
2016-06-24 17:53:27 +0200

    useless line of code erased

--- botclub.py
+++ botclub.py
@@ -34,7 +34,6 @@ bot.set_update_listener(listener)
 @bot.message_handler(commands=['start'])
 def command_benvinguda(m):
     cid = m.chat.id
-    IDlist = f.read()
     if not str(cid) in usuaris:
         usuaris.append(str(cid))
         f.write(str(cid)+"\n") #Here the user's ID will be registered to the file with all the user's ID
